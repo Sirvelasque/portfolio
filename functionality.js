1e2e221fc444f2b94d2a7726450e380fd8a2a45b
unknown <luispomare92@gmail.com>
2022-01-31 15:48:39 -0500

    Add functionality.js and create menu elements

---
+++ functionality.js
@@ -0,0 +1,13 @@
+const hamburguer = document.getElementById("hamburguer");
+
+//Created all elements of the menu
+let menuContainer = document.createElement("nav");
+let exit = document.createElement("div");
+let item1 = document.createElement("a");
+let itme2 = document.createElement("a");
+let item3 = document.createElement("a");
+
+//Give classes and values
+menuContainer.id = "menuContainer";
+//
+document.body.append(menuContainer);
