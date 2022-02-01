70537697088fb7476a0be04c3016531337355999
Sirvelasque <velasquezwalter10@yahoo.es>
2022-02-01 13:00:37 -0600

    set menu items for the nav bar

--- functionality.js
+++ functionality.js
@@ -3,11 +3,35 @@ const hamburguer = document.getElementById("hamburguer");
 //Created all elements of the menu
 let menuContainer = document.createElement("nav");
 let exit = document.createElement("div");
+let item1_container = document.createElement("div");
+let item2_container = document.createElement("div");
+let item3_container = document.createElement("div");
 let item1 = document.createElement("a");
-let itme2 = document.createElement("a");
+let item2 = document.createElement("a");
 let item3 = document.createElement("a");
+menuContainer.appendChild(item1_container);
+menuContainer.appendChild(item2_container);
+menuContainer.appendChild(item3_container);
 
 //Give classes and values
 menuContainer.id = "menuContainer";
+
+item1_container.classList.add('item_container')
+item1.classList.add('nav_text');
+item1.innerText='Portfolio';
+item1.href='https://www.google.pl';
+item1_container.appendChild(item1);
+
+item2_container.classList.add('item_container')
+item2.classList.add('nav_text');
+item2.innerText='About';
+item2.href='https://www.google.pl';
+item2_container.appendChild(item2);
+
+item3_container.classList.add('item_container')
+item3.classList.add('nav_text');
+item3.innerText='Contact';
+item3.href='https://www.google.pl';
+item3_container.appendChild(item3);
 //
 document.body.append(menuContainer);
